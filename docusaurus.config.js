ca12c139fbdc33e5eeffb43f0ef7ae496b505fc4
darin.baek <darin.baek@brique.co.kr>
2022-06-24 10:27:10 +0900

    set: 기본 설정 수정

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
-  url: "https://my-blog-site.com",
+  url: "https://eunju.github.io",
   baseUrl: "/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
